9f8afa0c3e5eedf99e239e58f72e339db896434c
Enrique Melgar <j_enriquem@hotmail.com>
2021-06-06 17:23:42 -0500

    MercadoLiebre responsive design first load

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static("public"));
+app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("views/home.html"));
+});
