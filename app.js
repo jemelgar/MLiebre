62ac3f8292b1634f0d059c9cceda033f7329e8c4
Enrique Melgar <j_enriquem@hotmail.com>
2021-06-20 16:47:10 -0500

    Correccion de puerto

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require("path");
+const app = express();
 const port = 3000;
 app.use(express.static("public"));
 
 app.listen(process.env.PORT || port, () =>
   console.log(`Servidor corriendo en puerto ${port}`)
 );
-
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("views/home.html"));
 });
