e3c0279d2b4a4fe5e524ef904bfed3b0946b072a
Enrique Melgar <j_enriquem@hotmail.com>
2021-06-20 16:40:18 -0500

    Práctica de mercado liebre terminada

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ app.listen(3000, () => console.log("Servidor corriendo en puerto 3000"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("views/home.html"));
 });
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve("views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve("views/login.html"));
+});
